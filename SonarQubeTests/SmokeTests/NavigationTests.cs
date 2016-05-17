33ac8354d54fd458cc7b2985ab3ec6e995b033a9
naw0jka <nawojka25@wp.pl>
2016-05-17 23:19:08 +0200

    3

---
+++ SonarQubeTests/SmokeTests/NavigationTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarQubeAutomation;
+
+namespace SonarQubeTests
+{
+    [TestClass]
+    public class NavigationTests : BaseForUser
+    {
+
+        [TestMethod]
+        public void GoToManageDashbords()
+        {
+            // Arrange
+            MainMenu.GoToCategory("Rules");
+            // Act
+            MainMenu.DropDownCategory("Dashboards", "Manage_Dashboards");
+            // Asssert
+            MainMenuAssertions.DropDownCategoryInMainMenuSelected("Manage_Dashboards");
+        }
+        
+    }
+}
