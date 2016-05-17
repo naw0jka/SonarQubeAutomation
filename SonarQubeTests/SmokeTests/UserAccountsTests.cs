7b1c35a1abcf35ffb4aefb9959194b1368b3c3b3
naw0jka <nawojka25@wp.pl>
2016-05-17 23:25:36 +0200

    3

---
+++ SonarQubeTests/SmokeTests/UserAccountsTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarQubeAutomation;
+
+namespace SonarQubeTests
+{
+    [TestClass]
+    public class UserAccountsTests : BaseForAdmin
+    {
+
+        [TestMethod]
+        public void CreateNewUser()
+        {
+            // Arrange
+            UsersPage.GoTo();
+            // Act
+            UsersPage.CreateNewUser("user1", "User1", "testsonarqube@qmail.com", "password");
+            // Asssert
+            UsersPageAssertions.NewUserCreated("User1");
+        }
+
+        [TestMethod]
+        public void DelateUser()
+        {
+            // Arrange
+            UsersPage.GoTo();
+            // Act
+            UsersPage.DeleteUser("user1");
+            // Asssert
+            UsersPageAssertions.UserDeleted("user1");
+        }
+    }
+}
